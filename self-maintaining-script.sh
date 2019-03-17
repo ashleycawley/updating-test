abc8fb5283e688bce4064fa0480cf611f3c4500f
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-17 17:45:39 +0000

    Update at Central Repo
    Lets see if this gets auto pushed

--- self-maintaining-script.sh
+++ self-maintaining-script.sh
@@ -71,6 +71,6 @@ else
     echo "Local & Remote md5sum are equal - This script is up to date."
 fi
 
-echo "Hello World! - `date`" >> ~/self-updating-test.txt
+echo "Hello New World in 2019! - `date`" >> ~/self-updating-test.txt
 
 exit 0
