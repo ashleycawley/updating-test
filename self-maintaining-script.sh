01664c61722a4897c6b16a08052064dcb03057ca
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-17 17:43:23 +0000

    2nd test run

--- self-maintaining-script.sh
+++ self-maintaining-script.sh
@@ -71,6 +71,6 @@ else
     echo "Local & Remote md5sum are equal - This script is up to date."
 fi
 
-echo "Hello World! - `date`" >> ~ self-updating-test.txt
+echo "Hello World! - `date`" >> ~/self-updating-test.txt
 
 exit 0
