0b00d3e5b433ca9f614c6e0cf06d64d3b0ebf23d
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-18 15:06:49 +0000

    Checking directories

--- self-maintaining-script.sh
+++ self-maintaining-script.sh
@@ -2,6 +2,17 @@
 
 # Variables
 
+CURDIR=`/bin/pwd`
+BASEDIR=$(dirname $0)
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+echo "========== DIAG =========="
+echo "CURDIR is $CURDIR"
+echo "BASEDIR is $BASEDIR"
+echo "ABSPATH is $ABSPATH"
+echo "ABSDIR is $ABSDIR"
+echo "========== DIAG =========="
+
 # URL to Source Repo which is checked (where the raw script code is hosted)
 UPDATE_SOURCE="https://raw.githubusercontent.com/ashleycawley/updating-test/master/self-maintaining-script.sh"
 
