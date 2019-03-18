09792688204dca15ea28e29e2bc6c9f4d64dd07e
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-18 15:26:51 +0000

    Streamlined and added notes

--- self-maintaining-script.sh
+++ self-maintaining-script.sh
@@ -2,11 +2,17 @@
 
 # Variables
 
+### Automatic Updater ###
+
+# The two variables below need configuring appropriately if you want this application to be
+# self-updating in the future. It can check a central repository for code updates, and checks
+# a third-party server to confirm if it should proceed with an update.
+
 # URL to Source Repo which is checked (where the raw script code is hosted)
 UPDATE_SOURCE="https://raw.githubusercontent.com/ashleycawley/updating-test/master/self-maintaining-script.sh"
 
 # URL to check to see if update should proceed
 UPDATE_AUTH_CHECK_URL="http://status.ashleycawley.co.uk/update-updating-test.txt"
 
-# 
+# Includes code from the automatic updater library
 source `dirname $0`/update.lib
